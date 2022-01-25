059607c77462b48b37fb52bfa34d3ad9e1970a56
Igor <sergienko339@gmail.com>
2022-01-25 17:51:58 +0200

    feat/actions interceptor

---
+++ src/shared/compareArrays.shared.ts
@@ -0,0 +1,20 @@
+const compareArrays = (arr1: [], arr2: []) => {
+    const array2Sorted = arr2.slice().sort();
+    const difference: [] = [];
+
+    const longest = arr1.length >= arr2.length ? arr1: arr2;
+    const shortest = arr1.length <= arr2.length ? arr1: arr2;
+
+    let result = true;
+
+    longest.forEach((el, i) => {
+       if(!shortest.includes(el)) {
+        difference.push(el);
+        result = false;
+       }
+    });
+
+    return {result, difference};
+};
+
+export default compareArrays;
