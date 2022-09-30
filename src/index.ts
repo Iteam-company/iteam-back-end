484291c738618168ad157e9b7d2a4356ea140b9c
Yevhen <jekasemenuk@icloud.com>
2022-09-30 13:48:54 +0300

    added root url

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,6 @@ export async function main(options: ApplicationConfig = {}) {
 	await server.boot();
 	await server.start();
 
-	console.log(`Server is running at ${process.env.PORT || 3000}`);
 	return server;
 }
 
