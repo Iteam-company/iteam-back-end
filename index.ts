01da77c13cecd16f5fc5778835e855b36be83300
Igor <sergienko339@gmail.com>
2022-10-18 18:35:03 +0300

    basic setup + install and remove swc

--- index.ts
+++ index.ts
@@ -1,4 +1,46 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { PORT, DB_PORT, DB_USER, DB_PWD, DB_HOST } from './env';
+import routes from './src/routes/index';
+class App {
+	public _app: express.Application;
+	public env: string;
+	public port: string | number;
 
-const app = express();
+	constructor() {
+		this._app = express();
+		this.env = 'development';
+		this.port = PORT || 3000;
+	}
+
+	connectMongoDb() {
+		try {
+			const connectionString = `mongodb${
+				DB_PORT || '+srv'
+			}://${DB_USER}:${DB_PWD}@${DB_HOST}/?retryWrites=true&w=majority`;
+			mongoose.connect(connectionString);
+		} catch (e) {
+			console.error(e);
+		}
+		return this;
+	}
+
+	connectRoutes() {
+		routes.forEach(({ router, path }) => {
+			this._app.use(path, router);
+		});
+		return this;
+	}
+
+	public async listen() {
+		this._app.listen(this.port, () => {
+			console.log(`======= ENV: ${this.env} ========`);
+			console.log(`🚀 App listening on the port ${this.port}`);
+			console.log(`🎮 http://localhost:${this.port}`);
+		});
+	}
+}
+
+const appInstance = new App().connectRoutes().connectMongoDb().listen();
+
+export default appInstance;
