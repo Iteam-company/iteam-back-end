2140ee6b1915c03e522e43346bccbe23e195da20
Igor <sergienko339@gmail.com>
2022-10-18 15:47:59 +0300

    clean-up loopback + set up esLint + swc

--- src/repositories/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './user.repository';
-export * from './allowed-emails.repository';
-export * from './projects.repository';
-export * from './logs.repository';
--- src/shared/getIdFromUrl.shared.ts
+++
@@ -1,8 +0,0 @@
-import { validate as uuidValidate } from 'uuid';
-
-const getIdFromUrl = (url: string) => {
-	const routeParts = url.split('/');
-	return routeParts.find((part: string) => uuidValidate(part));
-};
-
-export default getIdFromUrl;
