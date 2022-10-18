d8aad0e06b9bc8d29d7ca19fe379753ecca11a27
Igor <sergienko339@gmail.com>
2022-10-18 15:47:59 +0300

    clean-up loopback + set up esLint + swc

--- src/controllers/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './user.controller';
-export * from './allowed-emails.controller';
-export * from './projects.controller';
-export * from './files-upload.controller';
-export * from './logs.controller';
--- src/models/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './user.model';
-export * from './allowed-emails.model';
-export * from './projects.model';
-export * from './logs.model';
