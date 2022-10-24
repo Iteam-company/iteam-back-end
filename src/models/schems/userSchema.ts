8936e0d46461fb6a59ea89dcf5d803d1ca3f777a
Igor <sergienko339@gmail.com>
2022-10-24 18:20:33 +0300

    added auth controller + middleware + user model typization

--- src/models/schems/userSchema.ts
+++ src/models/schems/userSchema.ts
@@ -1,6 +1,14 @@
-import { Schema, model } from 'mongoose';
+import mongoose, { Schema, model } from 'mongoose';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import UserInterface, {
+	Roles,
+	Statuses,
+	WorkTypes,
+} from '../interfaces/user.interface';
+import { JWT_ACCES_SECRET_KEY, JWT_REFRESH_SERCET_KEY } from '../../../env';
 
-const user = new Schema({
+const user = new Schema<UserInterface>({
 	email: {
 		type: String,
 		required: true,
@@ -8,39 +16,94 @@ const user = new Schema({
 		trim: true,
 		lowercase: true,
 	},
-	password: { type: String, min: 8 },
+	password: { type: String, min: 8, required: false },
 	name: { type: String, required: true },
 	surname: { type: String, required: true },
 	role: {
 		type: String,
-		enum: ['ADMIN', 'HR', 'RECRUTIER', 'DEV', 'INTERN'],
-		default: 'DEV',
+		enum: [Roles.ADMIN, Roles.HR, Roles.RECRUTIER, Roles.DEV, Roles.INTERN],
+		default: Roles.DEV,
 	},
 	workType: {
 		type: String,
-		enum: ['REMOUTE', 'OFFICE', 'MIX'],
-		default: 'OFFICE',
+		enum: [WorkTypes.REMOUTE, WorkTypes.OFFICE, WorkTypes.MIX],
+		default: WorkTypes.OFFICE,
 	},
 	avatarUrl: { type: String, default: '/img/default.webp' },
 	file: { type: String },
 	phone: { type: String },
 	offerDay: { type: Date, default: Date.now() },
-	birthday: { type: String },
+	birthday: { type: Date },
 	expirience: { type: String },
 	skills: { type: String },
-	stack: { type: Array },
+	stack: [{ type: String }],
 	addres: { type: String },
 	city: { type: Number },
-	team: { type: Array, default: [] },
-	company: { type: Number },
+	team: [{ type: String, default: [] }],
+	company: { type: mongoose.Schema.Types.ObjectId, ref: 'Company' },
 	salary: { type: Number, default: 0 },
-	tokens: { type: Object },
-	links: { type: Array },
+	tokens: { type: Object, default: {} },
+	links: [{ type: String }],
 	status: {
 		type: String,
-		enum: ['ACTIVE', 'INACTIVE'],
-		default: 'ACTIVE',
+		enum: [Statuses.ACTIVE, Statuses.INACTIVE],
+		default: Statuses.ACTIVE,
 	},
 });
+
+user.methods.generateAccessToken = async function () {
+	try {
+		const token = jwt.sign(
+			{ _id: this._id },
+			JWT_ACCES_SECRET_KEY as string,
+			{
+				expiresIn: '12h',
+			}
+		);
+
+		this.tokens.accessToken = token;
+		await this.save();
+
+		return token;
+	} catch (e) {
+		console.log('err', e);
+	}
+};
+
+user.methods.generateRefreshToken = async function () {
+	try {
+		const token = jwt.sign(
+			{ _id: this._id },
+			JWT_REFRESH_SERCET_KEY as string,
+			{
+				expiresIn: '30d',
+			}
+		);
+
+		this.tokens.refreshToken = token;
+		await this.save();
+
+		return token;
+	} catch (e) {
+		console.log('err', e);
+	}
+};
+
+user.methods.hashPassword = async function () {
+	const rounds = 10;
+	try {
+		const hashedPassword = await bcrypt.hash(this.password, rounds);
+		this.password = hashedPassword;
+		await this.save();
+	} catch (e) {
+		console.log('err', e);
+	}
+};
+
+user.methods.removePassword = function () {
+	const userCopy = this.toObject();
+	delete userCopy.password;
+	return userCopy;
+};
 
 export default model('users', user);
