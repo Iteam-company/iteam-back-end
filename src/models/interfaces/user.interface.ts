3defa79adbd2d7d8d49e716d851d480f035f7f5b
Igor <sergienko339@gmail.com>
2022-10-25 20:12:57 +0300

    removed users field status

--- src/models/interfaces/user.interface.ts
+++ src/models/interfaces/user.interface.ts
@@ -22,7 +22,6 @@ interface UserInterface {
 	salary: number;
 	tokens: Tokens;
 	links: string[];
-	status: Statuses;
 
 	generateAccessToken: () => Promise<string>;
 	generateRefreshToken: () => Promise<string>;
