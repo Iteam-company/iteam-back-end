8dd7bad82f71769fb75a81026966c2798597e04a
Yevhen <jekasemenuk@icloud.com>
2022-10-26 16:44:51 +0300

    fixed types and schems

--- src/models/schems/suggestionSchema.ts
+++ src/models/schems/suggestionSchema.ts
@@ -1,3 +1,4 @@
+import { ObjectId } from 'mongodb';
 import { Schema, model } from 'mongoose';
 
 import SuggestionInterface from '../interfaces/suggestion.interface';
@@ -5,8 +6,13 @@ import SuggestionInterface from '../interfaces/suggestion.interface';
 const suggestion = new Schema<SuggestionInterface>({
 	title: { type: String, required: true },
 	text: { type: String },
-	variants: { type: Array, required: true, default: [] },
-	comments: { type: Array },
+	variants: [
+		{
+			type: { text: String, usersApproved: [{ type: ObjectId }] },
+			required: true,
+		},
+	],
+	comments: { type: { user: ObjectId, text: String } },
 	isApproved: { type: Boolean, default: false, required: true },
 	file: { type: String },
 	image: { type: String },
