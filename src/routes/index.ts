2b94f26708b3429e7c1e2cd1c2580c68ba04bc3e
Yevhen <jekasemenuk@icloud.com>
2022-10-26 17:15:46 +0300

    deleted test files + added nodemailder

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,13 +1,11 @@
 import authRouter from './auth.router';
 import projectRouter from './project.router';
 import userRouter from './user.router';
-import testRouter from './test.router';
 import applicationRouter from './application.router';
 
 const routers = [
 	{ path: '/project', router: projectRouter },
 	{ path: '/user', router: userRouter },
-	{ path: '/test', router: testRouter },
 	{ path: '/auth', router: authRouter },
 	{ path: '/application', router: applicationRouter },
 ];
