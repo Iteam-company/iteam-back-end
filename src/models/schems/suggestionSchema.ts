b2506dfc48b21d74ca200f9d5cffa89675f247cf
Yevhen <jekasemenuk@icloud.com>
2022-11-23 13:18:39 +0200

    added suggestions router + application pagination

--- src/models/schems/suggestionSchema.ts
+++ src/models/schems/suggestionSchema.ts
@@ -5,17 +5,17 @@ import SuggestionInterface from '../interfaces/suggestion.interface';
 
 const SuggestionSchema = new Schema<SuggestionInterface>({
 	title: { type: String, required: true },
-	text: { type: String },
+	text: { type: String, required: false },
 	variants: [
 		{
 			type: { text: String, usersApproved: [{ type: ObjectId }] },
 			required: true,
 		},
 	],
-	comments: { type: { user: ObjectId, text: String } },
-	isApproved: { type: Boolean, default: false, required: true },
-	file: { type: String },
-	image: { type: String },
+	comments: [{ type: { user: ObjectId, text: String } }],
+	isApproved: { type: Boolean, default: false, required: false },
+	file: { type: String, required: false },
+	image: { type: String, required: false },
 });
 
 const Suggestion = model('suggestions', SuggestionSchema);
