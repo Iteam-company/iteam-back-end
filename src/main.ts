79813f2eb3ad24c054c7f9a46561a591912e03ac
Misha Kravtsov <aniahucm@gmail.com>
2023-01-30 11:36:21 +0200

    connect config file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/app.module';
+import { PORT } from '@/constants/env';
 
 const start = async () => {
-  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(PORT, () => console.log(`server started at module ${PORT}`));
+  await app.listen(PORT ?? 5000, () =>
+    console.log(`server started at module ${PORT ?? 5000}`),
+  );
 };
+
+start();
