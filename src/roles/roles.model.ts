2553fed3d34bb922b014960271d4d5e6b998d0cf
Misha Kravtsov <aniahucm@gmail.com>
2023-02-01 16:39:38 +0200

    fixed intemediate table for link user with role

---
+++ src/roles/roles.model.ts
@@ -0,0 +1,53 @@
+import { User } from '@/users/users.model';
+import { UserRole } from '@/util-models/user-role.model';
+import { ApiProperty } from '@nestjs/swagger/dist/decorators';
+import {
+  BelongsToMany,
+  Column,
+  DataType,
+  Model,
+  Table,
+} from 'sequelize-typescript';
+
+interface RoleCreationAttributes {
+  value: string;
+  description: string;
+}
+
+@Table({
+  tableName: 'roles',
+})
+export class Role extends Model<Role, RoleCreationAttributes> {
+  @ApiProperty({ example: '1', description: 'unique identifier' })
+  @Column({
+    type: DataType.INTEGER,
+    unique: true,
+    autoIncrement: true,
+    primaryKey: true,
+  })
+  id: number;
+
+  @ApiProperty({
+    example: 'GUEST',
+    description: 'value of role',
+  })
+  @Column({
+    type: DataType.STRING,
+    unique: true,
+    allowNull: false,
+  })
+  value: string;
+
+  @ApiProperty({
+    example: 'that is base role',
+    description: 'description of role',
+  })
+  @Column({
+    type: DataType.STRING,
+    allowNull: false,
+  })
+  description: string;
+
+  @BelongsToMany(() => User, () => UserRole)
+  users: User[];
+}
