1ab982f2a168c4d592825b56f1f8cd40741e9026
Misha Kravtsov <aniahucm@gmail.com>
2023-02-01 16:37:53 +0200

    added promezhutochnyy

---
+++ src/roles/roles.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, Post, Body, Get, Param } from '@nestjs/common';
+import { CreateRoleDto } from './dto/create-role.dto';
+import { RolesService } from './roles.service';
+import { ApiTags } from '@nestjs/swagger/dist/decorators';
+
+@ApiTags('roles')
+@Controller('roles')
+export class RolesController {
+  constructor(private roleService: RolesService) {}
+
+  @Post()
+  create(@Body() dto: CreateRoleDto) {
+    return this.roleService.createRole(dto);
+  }
+
+  @Get('/:value')
+  getByValue(@Param('value') value: string) {
+    return this.roleService.getRoleByValue(value);
+  }
+}
