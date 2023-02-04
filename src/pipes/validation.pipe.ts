4735fa19e934caa702b1ee1114ea8f6e6e1568c6
Misha Kravtsov <aniahucm@gmail.com>
2023-02-04 13:52:36 +0200

    added set work type endpoint to users controller

--- src/pipes/validation.pipe.ts
+++ src/pipes/validation.pipe.ts
@@ -4,13 +4,15 @@ import { plainToClass } from 'class-transformer';
 import { validate } from 'class-validator';
 
 @Injectable()
-export class ValidationPipe implements PipeTransform<any> {
+export class ValidationBodyPipe implements PipeTransform<any> {
   async transform(value: any, metadata: ArgumentMetadata): Promise<any> {
+    if (metadata.type !== 'body') {
+      return value;
+    }
     const obj = plainToClass(metadata.metatype, value);
     const errors = await validate(obj);
 
     if (errors.length) {
-      console.log(errors, 'errors');
       const messages = errors.map((error) => {
         return `${error.property}: ${Object.values(error.constraints).join(
           ', ',
