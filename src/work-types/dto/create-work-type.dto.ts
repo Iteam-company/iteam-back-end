6de25ad2aa55bf60f8735e0f5017fa05e5cca75d
Misha Kravtsov <aniahucm@gmail.com>
2023-02-04 13:56:58 +0200

    added walidation to crea work type DTO

--- src/work-types/dto/create-work-type.dto.ts
+++ src/work-types/dto/create-work-type.dto.ts
@@ -2,10 +2,10 @@ import { ApiProperty } from '@nestjs/swagger/dist/decorators';
 import { IsString } from 'class-validator';
 
 export class CreateWorkTypeDto {
-  // @IsString({ message: 'must be a string value' })
+  @IsString({ message: 'must be a string value' })
   @ApiProperty({ example: 'OFFICE', description: 'work type enum value' })
   readonly value: string;
-  // @IsString({ message: 'must be a string value' })
+  @IsString({ message: 'must be a string value' })
   @ApiProperty({
     example: 'some description of worktype',
     description: 'some description of worktype',
