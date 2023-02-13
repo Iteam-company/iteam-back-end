0cce484a3c44605b4f4b1f3bdfe59e33c644475e
Misha Kravtsov <aniahucm@gmail.com>
2023-02-13 13:36:36 +0200

    fixed validation error return type

--- src/exceptions/validation.exception.ts
+++ src/exceptions/validation.exception.ts
@@ -4,7 +4,7 @@ export class ValidationException extends HttpException {
   messages;
 
   constructor(response) {
-    super(response, HttpStatus.BAD_REQUEST);
+    super(JSON.stringify(response), HttpStatus.BAD_REQUEST);
     this.messages = response;
   }
 }
